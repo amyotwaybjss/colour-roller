1353c42c753bbd974eef02d1fc576a0555083fc3
Amy Otway <amy.otway@bjss.com>
2024-02-06 11:16:12 +0000

    Added function to env to fix import statments

--- env.py
+++ env.py
@@ -4,7 +4,12 @@ wb = xl.load_workbook('colours2.xlsx')
 
 sheet_list = wb.sheetnames
 
-print(sheet_list)
+
+def print_env_message():
+    print("This is from env")
+
+# This function is added as without a function, the import statement does not work correctly;
+# Pulls in everything rather than just the respective variables.
 
 # this sheet will create our 'settings', load in sheet etc.
 # aka which sheets are in current spreadsheet and put them in a list.
